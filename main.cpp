8b1f77f0f5319e3664c5c0ba9fc64be5946d6887
qianduanXIAOHAOZI <zhengyh2018@gmail.com>
2020-08-30 10:52:38 +0800

    小改了一部分操作

--- main.cpp
+++ main.cpp
@@ -16,18 +16,18 @@ void cb() {
 void keyboardFunc(GLFWwindow* win, int key, int scancode, int action, int mods)
 {
     switch(key) {
-        case GLFW_KEY_UP:
-            camera.position.w += 0.4;
-            break;
-        case GLFW_KEY_DOWN:
-            camera.position.w -= 0.4;
-            break;
-        case GLFW_KEY_LEFT:
-            camera.position.x -= 0.4;
-            break;
-        case GLFW_KEY_RIGHT:
-            camera.position.x += 0.4;
-            break;
+//        case GLFW_KEY_UP:
+//            camera.position.w += 0.4;
+//            break;
+//        case GLFW_KEY_DOWN:
+//            camera.position.w -= 0.4;
+//            break;
+//        case GLFW_KEY_LEFT:
+//            camera.position.x -= 0.4;
+//            break;
+//        case GLFW_KEY_RIGHT:
+//            camera.position.x += 0.4;
+//            break;
         case GLFW_KEY_A:
             camera.rotation.y -= 0.1;
             break;
